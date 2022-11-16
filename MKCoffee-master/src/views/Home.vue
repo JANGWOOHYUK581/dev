3211ed559325b430c9619f1fd7f021b8a8b637f3
JANGWOOHYUK581 <84259126+JANGWOOHYUK581@users.noreply.github.com>
2022-11-16 20:50:32 +0900

    Add files via upload

---
+++ MKCoffee-master/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- Header Component -->
+    <div>
+       <Homehead />
+    </div>
+    <!-- Section Component -->
+    <div>
+     <Section />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Homehead from '../components/Homehead.vue'
+import Section from '../components/Section.vue'
+    export default {
+        name:'Home',
+        components:{
+          Homehead,
+          Section
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
